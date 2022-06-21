bef0dd694f2d60d9a7580ef2a7ff3a063469ea75
Alexey Duduk <lesha.duduk@gmail.com>
2022-06-21 19:20:26 +0400

    Update paths

--- configs/webpack/paths.js
+++ configs/webpack/paths.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-module.exports = {
-  src: path.resolve(__dirname, '../src'),
-  public: path.resolve(__dirname, '../public'),
-  build: path.resolve(__dirname, '../build'),
-};
+module.exports = (dirname) => ({
+  src: path.resolve(dirname, '../src'),
+  public: path.resolve(dirname, '../public'),
+  build: path.resolve(dirname, '../build'),
+});
--- configs/webpack/webpack.common.js
+++ configs/webpack/webpack.common.js
@@ -5,74 +5,78 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
 
-const paths = require('./paths');
+const createPaths = require('./paths');
 
-module.exports = (options) => ({
-  target: 'web',
-  entry: {
-    env: {
-      import: path.resolve(paths.src, 'env.ts'),
-      filename: 'public/env.js',
+module.exports = (options) => {
+  const paths = createPaths(options.root);
+
+  return {
+    target: 'web',
+    entry: {
+      env: {
+        import: path.resolve(paths.src, 'env.ts'),
+        filename: 'public/env.js',
+      },
+      main: path.resolve(paths.src, 'index.tsx'),
     },
-    main: path.resolve(paths.src, 'index.tsx'),
-  },
-  output: {
-    filename: 'static/js/[name].[contenthash].bundle.js',
-    chunkFilename: 'static/js/[name].[contenthash].bundle.js',
-    path: paths.build,
-    clean: true,
-  },
-  resolve: {
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: ['.tsx', '.ts', '.js', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)?$/,
-        loader: 'esbuild-loader',
-        options: {
-          loader: 'tsx',
-          target: 'es2015',
-          tsconfigRaw: require(options.tsConfigPath),
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
+    output: {
+      filename: 'static/js/[name].[contenthash].bundle.js',
+      chunkFilename: 'static/js/[name].[contenthash].bundle.js',
+      path: paths.build,
+      clean: true,
+    },
+    resolve: {
+      plugins: [new TsconfigPathsPlugin()],
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+    },
+    module: {
+      rules: [
         {
-          from: paths.public,
-          globOptions: {
-            ignore: ['**/*.html'],
+          test: /\.(js|jsx|ts|tsx)?$/,
+          loader: 'esbuild-loader',
+          options: {
+            loader: 'tsx',
+            target: 'es2015',
+            tsconfigRaw: require(options.tsConfigPath),
           },
-          to: path.resolve(paths.build, 'public'),
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+          type: 'asset/inline',
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      showErrors: true,
-      cache: true,
-      template: path.resolve(paths.public, 'index.html'),
-      chunks: [
-        'env',
-        'main',
-      ]
-    }),
-    new webpack.ProvidePlugin({
-      React: 'react',
-    }),
-    new ForkTsCheckerPlugin(),
-  ],
-});
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: paths.public,
+            globOptions: {
+              ignore: ['**/*.html'],
+            },
+            to: path.resolve(paths.build, 'public'),
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        showErrors: true,
+        cache: true,
+        template: path.resolve(paths.public, 'index.html'),
+        chunks: [
+          'env',
+          'main',
+        ]
+      }),
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+      new ForkTsCheckerPlugin(),
+    ],
+  };
+};
